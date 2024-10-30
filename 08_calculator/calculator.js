e81895c89936f5e21d598fecfaebb32241993937
Andrew Morgan <23585699+seaparks@users.noreply.github.com>
2024-10-30 14:38:47 -0400

    basic calc functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
+};  
+
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total+current,0);
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  return array.reduce((total, current) => total*current,1);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  for (let i = n; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
