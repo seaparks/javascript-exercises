998444bcf91631b81e7a6c779d26673213f8e958
Andrew Morgan <23585699+seaparks@users.noreply.github.com>
2024-10-30 15:38:02 -0400

    fibonacci, havent seen you in years

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 0){ return 0 };
+    if (n < 0){ return "OOPS" };
 
+    let values = [0,1];
+
+    for (let i = 2; i <= n; i++){
+        values.push( values[i-1] + values[i-2] );
+    }
+
+    return values[n];
 };
 
 // Do not edit below this line
